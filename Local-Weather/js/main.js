a3d3d13e1c8b414d984e2e99527324f07d18cb61
crisgon <kristiano.gon@gmail.com>
2018-03-26 12:52:49 -0300

    Code refactoring

--- Local-Weather/js/main.js
+++ Local-Weather/js/main.js
@@ -1,62 +1,56 @@
-function $ (element) {
-  return document.querySelector(element);
-}
+(function(win, doc){
+  'use strict';
 
-city = $('[data-js="city"]');
-temp = $('[data-js="temp"]');
-description = $('[data-js="description"]');
-img = $('img');
-console.log(img);
-
-if("geolocation" in navigator)
-  getLocation();
-else
-  alert('Browser does not support geolocation')
-
+  const $ = doc.querySelector.bind(document);
+  const $city = $('[data-js="city"]');
+  const $temp = $('[data-js="temp"]');
+  const $description = $('[data-js="description"]');
+  const img = $('img');
   
+function app() {
+  return {
+    init() {
+      if("geolocation" in navigator)
+        return app().getLocation();
 
-  function getLocation () {
-  navigator.geolocation.getCurrentPosition(position => {
-    const lat = position.coords.latitude;
-    const long = position.coords.longitude;
-    conection (lat,long);
-  });
+      return alert('Browser does not support geolocation')
+    },
+
+    getLocation() {
+      navigator.geolocation.getCurrentPosition(position => {
+        const lat = position.coords.latitude;
+        const long = position.coords.longitude;
+        this.conection(lat, long);
+      });
+    },
+    
+    async conection(lat,long) {
+      const response = await fetch(`https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${long}`);
+      const data = await response.json();
+      app().changeHtml(data);
+    },
+    
+    changeHtml(response) {
+      $city.innerHTML = `${response.name} ${response.sys.country}`;
+      $description.innerHTML = `${response.weather[0].main} ${response.weather[0].description}`;
+      $temp.innerHTML = `${Math.round(response.main.temp)} C°`;
+      var image = this.icon(response.weather[0].main);
+      img.setAttribute('src', image);
+    },
+    
+    icon(status) {
+      return {
+        Drizzle:	'https://image.flaticon.com/icons/svg/146/146526.svg',
+        Clouds: 'https://image.flaticon.com/icons/svg/146/146517.svg',
+        Rain: 'https://image.flaticon.com/icons/svg/146/146526.svg',
+        Snow: 'https://image.flaticon.com/icons/svg/146/146520.svg',
+        Clear: 'https://image.flaticon.com/icons/svg/146/146519.svg',
+        Mist: 'https://image.flaticon.com/icons/svg/146/146518.svg',
+        Storm: 'https://image.flaticon.com/icons/svg/146/146523.svg' 
+      }[status];
+    },
+  };
 }
-
-function conection (lat,long) {
-  const apiConect = new XMLHttpRequest();
-  apiConect.open('GET', 'https://fcc-weather-api.glitch.me/api/current?lat='+ lat +'&lon=' + long);
-  apiConect.send();
-  apiConect.addEventListener('load', function() {
-  const response = JSON.parse(apiConect.responseText);
-  changeHtml(response);
-});
-}
-
-
-function changeHtml (response) {
-  city.innerHTML = response.name + ", " + response.sys.country;
-  description.innerHTML = response.weather[0].main + ", " + response.weather[0].description;
-  temp.innerHTML = Math.round(response.main.temp) + " C°";
-  var image = icon(response.weather[0].main);
-  img.setAttribute('src', image);
-  console.log(response);
-  console.log(response.weather[0].description);
-}
-
-
-
-
-function icon (status) {
-  var icons = {
-    Drizzle:	'https://image.flaticon.com/icons/svg/146/146526.svg',
-    Clouds: 'https://image.flaticon.com/icons/svg/146/146517.svg',
-    Rain: 'https://image.flaticon.com/icons/svg/146/146526.svg',
-    Snow: 'https://image.flaticon.com/icons/svg/146/146520.svg',
-    Clear: 'https://image.flaticon.com/icons/svg/146/146519.svg',
-    Mist: 'https://image.flaticon.com/icons/svg/146/146518.svg',
-    Storm: 'https://image.flaticon.com/icons/svg/146/146523.svg' 
-  }
-
-  return icons[status];
-}
+app().init();
+  
+}(window, document));
